6df4fbf8afca3585a427faf03f1b8c62e9852f15
Haerin <soundzrinjhr@gmail.com>
2022-04-08 15:24:44 +0900

    python

---
+++ python/tuple copy.py
@@ -0,0 +1,8 @@
+t = ()
+xy = (2560, 1440)
+color = 129, 147, 216
+print(xy + color)
+print(xy * 2)
+print(xy)
+print(color)
+print(type(xy))
---
+++ python/tuple3_.py
@@ -0,0 +1,3 @@
+print(list('Happy'))
+print(list((123,456,789)))
+print(tuple([123,456,789]))
