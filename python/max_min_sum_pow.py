32c2556f084e23aa351569db2008ab824ed83819
Haerin <soundzrinjhr@gmail.com>
2022-06-10 15:19:47 +0900

    06.10

---
+++ python/max_min_sum_pow.py
@@ -0,0 +1,5 @@
+nums = [1, 5, -2, 0 , 6]
+print(nums, "중 가장 큰 값 ", max(nums))
+print(nums, "중 가장 작은 값 ", min(nums))
+print(nums, "합계 ", sum(nums))
+print("2의 10승 ", pow(2, 10))
---
+++ python/round.py
@@ -0,0 +1,6 @@
+pi = 3.141592
+print(pi,"의 소수점 1자리 반올림 ", round(pi))
+print(pi,"의 소수점 1자리 반올림 ", round(pi, 0))
+print(pi,"의 소수점 2자리 반올림 ", round(pi, 1))
+print(pi,"의 소수점 3자리 반올림 ", round(pi, 2))
+print(pi,"의 소수점 4자리 반올림 ", round(pi, 3))
