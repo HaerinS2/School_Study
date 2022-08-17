fac95ece53a12aa7e630c6858cce5fc91bb58907
Haerin <soundzrinjhr@gmail.com>
2022-08-17 21:19:12 +0900

    2022.08.17

---
+++ web/js/clock.js
@@ -0,0 +1,35 @@
+function tick(){
+    var month, date, hours, minutes, seconds, ap;
+    var intYear, intDate, intMon, intHours, intMinutes, intSeconds;
+    var today = new Date();
+
+    intYear = today.getFullYear();
+    intMon = today.getMonth()+1;
+    intDate = today.getDate();
+    intHours = today.getHours();
+    intMinutes = today.getMinutes();
+    intSeconds = today.getSeconds();
+
+    if(intMon<10){
+        month="0"+intMon;
+    } else{mon = intMon;}
+    
+    if(intDate<10){
+        date="0"+intDate;
+    } else{date = intDate;}
+
+    if(intHours==0){hours = "12"; ap = "AM";
+    } 
+    else if(intHours<12){hours = intHours + ":"; ap = "AM"; 
+    }
+    else if(intHours==12){hours = "12" + ":"; ap = "PM"; 
+    }
+    else{intHours = intHours-12; hours = intHours + ":"; ap = "PM"}
+    if(intMinutes < 10){minutes = "0"+intMinutes+":";}
+    else{minutes=intMinutes+":";}
+    if(intSeconds < 10){seconds = "0" + intSeconds;}
+    else{seconds=intSeconds;}
+    TimeString = intYear + "-" + month + "-" + "-" + date + "-" + " " + hours + minutes + seconds + " " + ap;
+    window.setTimeout("tick();", 100);
+    window.onload = tick;
+}
